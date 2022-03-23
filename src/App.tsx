b49d09c64d1c0487c4db51d0c7344dbe906998e7
hyunbell <man.of.backend@gmail.com>
2022-03-23 14:34:12 +0900

    :tada: feat: init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from './App.style';
+import Board from './pages/Board';
+const App = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <BrowserRouter basename='Board'>
+                <Routes>
+                    <Route path="/" element={<Board />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    )
+}
+
+export default App;
