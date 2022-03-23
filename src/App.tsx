bd85b886b42f54c780522b9051e0ef6cf1a90362
hyunbell <man.of.backend@gmail.com>
2022-03-23 16:02:42 +0900

    :shirt: chore: lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+
 import { GlobalStyle, theme } from './App.style';
-import Board from './pages/Board';
+import Board from './pages/FindaBoard';
 const App = () => {
-    return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <BrowserRouter basename='Board'>
-                <Routes>
-                    <Route path="/" element={<Board />}></Route>
-                </Routes>
-            </BrowserRouter>
-        </ThemeProvider>
-    )
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter basename="Board">
+        <Routes>
+          <Route path="/" element={<Board />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
 
 export default App;
