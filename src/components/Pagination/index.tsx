ee63297c4ac33d7b916768502344766d6822dae4
hyunbell <man.of.backend@gmail.com>
2022-04-01 12:05:20 +0900

    feat: Add pagination component

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,45 @@
+import React, { useMemo } from 'react';
+import { Text } from '@src/components';
+import cn from 'classnames';
+
+import StyledPagination from './style';
+
+export interface PaginationProps {
+  className?: string;
+  pageSize: number;
+  totalLength: number;
+  currentPage: number;
+  onDispatch: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+  className,
+  pageSize,
+  totalLength,
+  currentPage,
+  onDispatch,
+}) => {
+  const totalPage = useMemo<number>(
+    () => Math.ceil(totalLength / pageSize),
+    [totalLength, pageSize]
+  );
+
+  return (
+    <StyledPagination className={cn(`_PAGINATION_`, className)}>
+      {[...Array(totalPage)].map((_, index) => (
+        <Text
+          className={cn(`pagination-number`, index + 1 === currentPage ? 'clicked' : '')}
+          fontColor="gray"
+          fontSize="medium"
+          fontWeight="regular"
+          key={index}
+          handleClick={() => onDispatch(index + 1)}
+        >
+          {index + 1}
+        </Text>
+      ))}
+    </StyledPagination>
+  );
+};
+
+export default Pagination;
---
+++ src/components/Pagination/style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const StyledPagination = styled.div`
+  .pagination-number {
+    cursor: pointer;
+    display: inline !important;
+    margin: 0rem 1rem;
+    transition: 0.5s;
+
+    &.clicked {
+      color: ${({ theme }) => theme.color.blue5};
+    }
+
+    &:hover {
+      color: #333;
+    }
+  }
+`;
+
+export default StyledPagination;
