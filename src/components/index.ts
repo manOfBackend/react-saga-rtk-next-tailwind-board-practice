3284b1f7f86bd8e3c5ac4a14a67ce23507be2667
hyunbell <man.of.backend@gmail.com>
2022-04-01 12:08:55 +0900

    chore: Add index (components)

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from './Button';
+export { default as FixedBottomButton } from './FixedBottomButton';
+export { default as Pagination } from './Pagination';
+export { default as Table } from './Table';
+export { default as Text } from './Text';
