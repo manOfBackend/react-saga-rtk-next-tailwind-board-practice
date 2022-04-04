4f2037d6512c4029d9b809312e15f035773e88fd
hyunbell <man.of.backend@gmail.com>
2022-04-04 13:51:26 +0900

    fix: 게시판 글 클릭시 saga action 호출 정리

--- src/pages/FindaBoard/index.tsx
+++ src/pages/FindaBoard/index.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { FixedBottomButton, Pagination, Table, Text } from '@src/components';
+import Stack from '@src/components/Stack';
 import { RootState } from '@src/configureStore';
+import { detailActions } from '@src/features/detail/detailSlice';
 import { postsActions } from '@src/features/posts/postsSlice';
+import { css } from 'styled-components';
 
 import { useInternalRouter } from '../Routing';
 
@@ -15,56 +18,68 @@ const FindaBoard = () => {
 
   useEffect(() => {
     dispatch(postsActions.getPosts({ page }));
-  }, [page]);
+  }, [page, dispatch]);
 
   return (
     <div className="finda-board">
       <section className="finda-board-wrapper">
-        <Table>
-          <thead>
-            <tr>
-              <th>
-                <Text align="center" fontSize="small">
-                  번호
-                </Text>
-              </th>
-              <th>
-                <Text align="center" fontSize="small">
-                  작성자
-                </Text>
-              </th>
-              <th>
-                <Text align="center" fontSize="small">
-                  제목
-                </Text>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {posts?.posts.map((post) => (
-              <tr key={post.id}>
-                <td>
-                  <Text align="center" fontSize="xx-small">
-                    {post.id}
+        <Stack
+          css={css`
+            margin-top: 20px;
+          `}
+        >
+          <Table>
+            <thead>
+              <tr>
+                <th>
+                  <Text align="center" fontSize="small">
+                    번호
                   </Text>
-                </td>
-                <td>
-                  <Text align="center" fontSize="xx-small">
-                    {post.writer}
+                </th>
+                <th>
+                  <Text align="center" fontSize="small">
+                    작성자
                   </Text>
-                </td>
-                <td>
-                  <Text align="center" fontSize="xx-small">
-                    {post.title}
+                </th>
+                <th>
+                  <Text align="center" fontSize="small">
+                    제목
                   </Text>
-                </td>
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-        <div className="finda-board-pagination-wrapper">
-          <Pagination pageSize={16} totalLength={120} currentPage={page} onDispatch={setPage} />
-        </div>
+            </thead>
+            <tbody>
+              {posts?.map((post) => (
+                <tr
+                  key={post.id}
+                  onClick={() => {
+                    dispatch(detailActions.getDetail({ id: post.id }));
+                    router.push(`/detail`);
+                  }}
+                >
+                  <td>
+                    <Text align="center" fontSize="xx-small">
+                      {post.id}
+                    </Text>
+                  </td>
+                  <td>
+                    <Text align="center" fontSize="xx-small">
+                      {post.writer}
+                    </Text>
+                  </td>
+                  <td>
+                    <Text align="center" fontSize="xx-small">
+                      {post.title}
+                    </Text>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <div className="finda-board-pagination-wrapper">
+            <Pagination pageSize={10} totalLength={50} currentPage={page} onDispatch={setPage} />
+          </div>
+        </Stack>
       </section>
       <FixedBottomButton onClick={() => router.push('/write')}>글쓰기</FixedBottomButton>
     </div>
