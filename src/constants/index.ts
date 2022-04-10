75ba99ad26986a4b2c212b31aca2940d632126c1
hyunbell <man.of.backend@gmail.com>
2022-04-10 15:11:06 +0900

    refactor: fontSize 이동

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,2 @@
+export type { default as FontSize } from './fontSize';
 export { default as REQUEST_URL } from './requestUrl';
--- src/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export type { default as FontSize } from './fontSize';
